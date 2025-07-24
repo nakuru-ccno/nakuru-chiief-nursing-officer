1a66d910f4608c4b3a4826ee8a2caf6cd2f5bd51
CCNO Admin <ccno@nakurucountychiefnursingofficer.site>
2025-07-24 20:47:00 +0300

    Fix missing calendarUtils import for react-big-calendar

---
+++ src/lib/calendarUtils.ts
@@ -0,0 +1,17 @@
+import { dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
+  getDay,
+  locales,
+});
+
+export { localizer };
