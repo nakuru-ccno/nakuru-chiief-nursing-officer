ea9c3a25034ea9c6c4d912480d788efcd2e9d8bb
nakuru-ccno <nakurucountychiefnursingoffice@gmail.com>
2025-07-28 11:58:26 +0300

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,21 +1,23 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
-import { ThemeProvider } from "@/components/theme-provider";
-import MainNavbar from "@/components/MainNavbar";
-import CountyHeader from "@/components/CountyHeader";
+import MainNavbar from "./MainNavbar";
+import CountyHeader from "./CountyHeader";
 
 const MainLayout = () => {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="nakuru-theme">
-      <div className="min-h-screen bg-background text-foreground flex flex-col">
-        <MainNavbar />
-        <CountyHeader />
-        <main className="flex-1 p-4">
-          <Outlet />
-        </main>
-      </div>
-    </ThemeProvider>
+    <div className="min-h-screen bg-gray-100">
+      {/* ✅ County Logo and Header at the very top */}
+      <CountyHeader />
+
+      {/* ✅ Then the navigation menu below it */}
+      <MainNavbar />
+
+      {/* ✅ Main page content */}
+      <main className="p-4 max-w-7xl mx-auto">
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
 export default MainLayout;
-
